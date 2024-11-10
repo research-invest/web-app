ee41b1d19b13a07d41cbf4a3564c4dc93b00db79
rocky <88kg@mail.ru>
2024-11-10 12:13:17 +0300

    сделки

--- app/Orchid/Layouts/Trading/Deals/ListLayout.php
+++ app/Orchid/Layouts/Trading/Deals/ListLayout.php
@@ -31,50 +31,16 @@ class ListLayout extends Table
                     ->route('platform.trading.deal.edit', $trade)
                 ),
 
-            TD::make('position_type', 'Тип позиции'),
+            TD::make('currency', 'Пара')
+                ->render(fn (Trade $trade) => $trade->currency->name),
+
+            TD::make('position_type', 'Тип'),
+            TD::make('position_size', 'Размер'),
             TD::make('entry_price', 'Цена входа'),
             TD::make('leverage', 'Плечо'),
-            TD::make('currency', 'Валюта')
-                ->render(fn (Trade $trade) => $trade->currency->name),
             TD::make('status', 'Статутс'),
 
-//            TD::make('is_active', '')
-//                ->render(fn (Trade $trade) =>
-//                     $trade->currency->name
-//                        ? '<i class="text-success">●</i>'
-//                        : '<i class="text-danger">●</i>'
-//                ),
-//
-//            TD::make('is_temp_card', 'Временная карта')
-//                ->popover('Добавленная в заявке')
-//                ->render(fn (Trade $trade) =>
-//                     $trade->is_temp_card
-//                        ? '<i class="text-success">●</i> Да'
-//                        : '<i class="text-danger">●</i> Нет'
-//                ),
-//
-//            TD::make('currency_id', 'Банк')
-//                ->render(fn(Trade $trade) => $trade->currency->name
-//                ),
-//
-//            TD::make('card', 'Номер')
-//                ->render(function (Trade $trade) {
-//                    return $trade->format_card;
-//                }),
-//
-//            TD::make('fio', 'ФИО'),
-//            TD::make('description', 'Описание'),
-//            TD::make('status', 'Статус')->render(fn (Trade $trade) =>
-//                $trade->getStatusName()
-//            ),
-//
-//            TD::make('last_used_at', 'Последнее использование')
-//                ->sort()
-//                ->render(fn (Trade $trade) =>
-//                    $trade->last_used_at?->toDateTimeString()
-//                ),
-
-            TD::make('created_at', 'Дата добавления')
+            TD::make('created_at', 'Дата открытия')
                 ->sort()
                 ->render(fn (Trade $trade) =>
                     $trade->created_at->toDateTimeString()
