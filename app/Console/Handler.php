5d6d65c172b082cfa6d305cf724fdb4b031e76a3
rocky <88kg@mail.ru>
2024-11-10 11:53:37 +0300

    сделки

--- app/Console/Handler.php
+++ app/Console/Handler.php
@@ -9,7 +9,7 @@ class Handler
 {
     public function __invoke(Schedule $schedule): void
     {
-        $schedule->command(UpdateCurrencies::class)->everyTenMinutes();
+        $schedule->command(UpdateCurrencies::class)->everyTwoMinutes();
 
     }
 }
