2444d01af80f67698efaebf5541c1c57fc01f518
rocky <88kg@mail.ru>
2024-12-12 13:08:43 +0300

    пуллятор

---
+++ pull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git pull
+chown -R www-data:www-data /var/www/web-app/storage
+chown -R www-data:www-data /var/www/web-app/bootstrap/cache
+chmod -R 775 /var/www/web-app/storage
+chmod -R 775 /var/www/web-app/bootstrap/cache
