6c61bff29b02f689e375027a4e13e6f607808615
rocky <88kg@mail.ru>
2024-12-12 22:15:32 +0300

    анализ топов

--- pull.sh
+++ pull.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 git pull
+
+php artisan migrate --force
+php artisan optimize
+
 chown -R www-data:www-data /var/www/web-app/storage
 chown -R www-data:www-data /var/www/web-app/bootstrap/cache
 chmod -R 775 /var/www/web-app/storage
 chmod -R 775 /var/www/web-app/bootstrap/cache
 
-php artisan migrate --force
-php artisan optimize
+
