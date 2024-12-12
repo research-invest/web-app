9b80f07918f0991b281a3db25c4c3e67b03fa473
rocky <88kg@mail.ru>
2024-12-12 22:01:32 +0300

    история топов

--- pull.sh
+++ pull.sh
@@ -5,5 +5,5 @@ chown -R www-data:www-data /var/www/web-app/bootstrap/cache
 chmod -R 775 /var/www/web-app/storage
 chmod -R 775 /var/www/web-app/bootstrap/cache
 
-php artisan migrate
+php artisan migrate --force
 php artisan optimize
