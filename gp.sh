69ca70762decdee4e1e239e6a101381a9691d1b0
rocky <88kg@mail.ru>
2025-02-20 11:31:09 +0300

    queue:restart

--- gp.sh
+++ gp.sh
@@ -3,10 +3,14 @@ git pull
 
 php artisan migrate --force
 php artisan optimize
+php artisan queue:restart
 
 chown -R www-data:www-data /var/www/web-app/storage
 chown -R www-data:www-data /var/www/web-app/bootstrap/cache
 chmod -R 775 /var/www/web-app/storage
 chmod -R 775 /var/www/web-app/bootstrap/cache
+
+
+#supervisorctl restart worker_cryptosasha:*
 
 
