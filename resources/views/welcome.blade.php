bf15192d3031f2a364de5bf1084f5ff9f64ae24a
rocky <88kg@mail.ru>
2025-03-01 19:27:02 +0300

    калькулятор

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <link rel="icon" href="/assets/calc/favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Калькулятор позиции</title>
-    <script type="module" crossorigin src="/assets/calc/assets/index-ixI0SsmC.js"></script>
-    <link rel="stylesheet" crossorigin href="/assets/calc/assets/index-keVrlctQ.css">
+    <script type="module" crossorigin src="/assets/calc/assets/index-DnBiqHTQ.js"></script>
+    <link rel="stylesheet" crossorigin href="/assets/calc/assets/index-1UYHzlXy.css">
 </head>
 <body>
 <div id="app"></div>
