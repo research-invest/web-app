f757429bdd2a18a4880757c0925857a03a65d5ae
rocky <88kg@mail.ru>
2025-03-04 20:49:49 +0300

    сделки фандинга

--- app/Orchid/Layouts/User/UserApiKeysLayout.php
+++ app/Orchid/Layouts/User/UserApiKeysLayout.php
@@ -20,52 +20,51 @@ class UserApiKeysLayout extends Rows
     {
         return [
             Group::make([
-                Input::make('user.mexc_secret_key')
-                    ->type('text')
-                    ->max(255)
-                    ->title('mexc secret key'),
-
                 Input::make('user.mexc_api_key')
                     ->type('text')
                     ->max(255)
                     ->title('mexc api key'),
+
+                Input::make('user.mexc_secret_key')
+                    ->type('text')
+                    ->max(255)
+                    ->title('mexc secret key'),
             ]),
 
             Group::make([
-                Input::make('user.bybit_secret_key')
-                    ->type('text')
-                    ->max(255)
-                    ->title('bybit secret key'),
-
                 Input::make('user.bybit_api_key')
                     ->type('text')
                     ->max(255)
                     ->title('bybit api key'),
 
-            ]),
-            Group::make([
-                Input::make('user.binance_secret_key')
+                Input::make('user.bybit_secret_key')
                     ->type('text')
                     ->max(255)
-                    ->title('binance secret key'),
+                    ->title('bybit secret key'),
+            ]),
 
+            Group::make([
                 Input::make('user.binance_api_key')
                     ->type('text')
                     ->max(255)
                     ->title('binance api key'),
 
+                Input::make('user.binance_secret_key')
+                    ->type('text')
+                    ->max(255)
+                    ->title('binance secret key'),
             ]),
 
             Group::make([
-                Input::make('user.bigx_secret_key')
-                    ->type('text')
-                    ->max(255)
-                    ->title('bigx secret key'),
-
                 Input::make('user.bigx_api_key')
                     ->type('text')
                     ->max(255)
                     ->title('bigx api key'),
+
+                Input::make('user.bigx_secret_key')
+                    ->type('text')
+                    ->max(255)
+                    ->title('bigx secret key'),
             ]),
 
 
