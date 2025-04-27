d62795736cf15c93a2eaf946c553bc17f9b95ea2
rocky <88kg@mail.ru>
2025-04-27 14:11:19 +0300

    отчет в сделках ф

--- gp.sh
+++ gp.sh
@@ -5,10 +5,12 @@ php artisan migrate --force
 php artisan optimize
 php artisan queue:restart
 
-chown -R www-data:www-data /var/www/web-app/storage
-chown -R www-data:www-data /var/www/web-app/bootstrap/cache
-chmod -R 775 /var/www/web-app/storage
-chmod -R 775 /var/www/web-app/bootstrap/cache
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+chown -R www-data:www-data "$SCRIPT_DIR/storage"
+chown -R www-data:www-data "$SCRIPT_DIR/bootstrap/cache"
+chmod -R 775 "$SCRIPT_DIR/storage"
+chmod -R 775 "$SCRIPT_DIR/bootstrap/cache"
 
 
 #supervisorctl restart worker_cryptosasha:*
