6364ec99e3d18fc9f8096d536230d1cb2c16a3c4
rocky <88kg@mail.ru>
2025-05-24 23:11:30 +0300

    сделки фандинга

--- app/Orchid/Layouts/Trading/Deals/Funding/InfoBlock.php
+++ app/Orchid/Layouts/Trading/Deals/Funding/InfoBlock.php
@@ -47,7 +47,7 @@ class InfoBlock extends Legend
             Sight::make('pnl_before_funding', 'pnl_before_funding'),
             Sight::make('total_pnl', 'pnl'),
             Sight::make('pnl_percent', 'pnl %')->render(function (FundingDeal $trade) {
-                return MathHelper::getPercentOfNumber($trade->initial_margin, $trade->total_pnl);
+                return MathHelper::getPercentOfNumber($trade->total_pnl, $trade->initial_margin,);
             }),
             Sight::make('roi_percent', 'roi'),
             Sight::make('pre_funding_volatility', 'Индекс волатильности'),
