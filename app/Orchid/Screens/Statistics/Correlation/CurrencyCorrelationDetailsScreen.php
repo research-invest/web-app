d873fec5a8f471555f572f5538d1b1827aa5523f
rocky <88kg@mail.ru>
2025-06-09 14:52:34 +0300

    корреляция

--- app/Orchid/Screens/Statistics/Correlation/CurrencyCorrelationDetailsScreen.php
+++ app/Orchid/Screens/Statistics/Correlation/CurrencyCorrelationDetailsScreen.php
@@ -114,25 +114,93 @@ class CurrencyCorrelationDetailsScreen extends Screen
                                 'valueDecimals' => 8
                             ]
                         ],
+                        // BTC корреляции
                         [
-                            'name' => 'Корреляция с BTC',
-                            'data' => $this->chartData['btcCorrelation'],
+                            'name' => 'BTC 4H',
+                            'data' => $this->chartData['btcCorrelation4h'],
                             'yAxis' => 1,
-                            'color' => '#FFC107',
+                            'color' => '#FFA000',
+                            'tooltip' => [
+                                'valueSuffix' => '%',
+                                'valueDecimals' => 2
+                            ],
+                            'dashStyle' => 'ShortDash'
+                        ],
+                        [
+                            'name' => 'BTC 12H',
+                            'data' => $this->chartData['btcCorrelation12h'],
+                            'yAxis' => 1,
+                            'color' => '#FF6F00',
+                            'tooltip' => [
+                                'valueSuffix' => '%',
+                                'valueDecimals' => 2
+                            ],
+                            'dashStyle' => 'ShortDot'
+                        ],
+                        [
+                            'name' => 'BTC 24H',
+                            'data' => $this->chartData['btcCorrelation24h'],
+                            'yAxis' => 1,
+                            'color' => '#E65100',
                             'tooltip' => [
                                 'valueSuffix' => '%',
                                 'valueDecimals' => 2
                             ]
                         ],
                         [
-                            'name' => 'Корреляция с ETH',
-                            'data' => $this->chartData['ethCorrelation'],
+                            'name' => 'BTC Среднее',
+                            'data' => $this->chartData['btcCorrelationAvg'],
+                            'yAxis' => 1,
+                            'color' => '#FFC107',
+                            'tooltip' => [
+                                'valueSuffix' => '%',
+                                'valueDecimals' => 2
+                            ],
+                            'lineWidth' => 3
+                        ],
+                        // ETH корреляции
+                        [
+                            'name' => 'ETH 4H',
+                            'data' => $this->chartData['ethCorrelation4h'],
+                            'yAxis' => 1,
+                            'color' => '#00C853',
+                            'tooltip' => [
+                                'valueSuffix' => '%',
+                                'valueDecimals' => 2
+                            ],
+                            'dashStyle' => 'ShortDash'
+                        ],
+                        [
+                            'name' => 'ETH 12H',
+                            'data' => $this->chartData['ethCorrelation12h'],
+                            'yAxis' => 1,
+                            'color' => '#00B8D4',
+                            'tooltip' => [
+                                'valueSuffix' => '%',
+                                'valueDecimals' => 2
+                            ],
+                            'dashStyle' => 'ShortDot'
+                        ],
+                        [
+                            'name' => 'ETH 24H',
+                            'data' => $this->chartData['ethCorrelation24h'],
+                            'yAxis' => 1,
+                            'color' => '#00BFA5',
+                            'tooltip' => [
+                                'valueSuffix' => '%',
+                                'valueDecimals' => 2
+                            ]
+                        ],
+                        [
+                            'name' => 'ETH Среднее',
+                            'data' => $this->chartData['ethCorrelationAvg'],
                             'yAxis' => 1,
                             'color' => '#4CAF50',
                             'tooltip' => [
                                 'valueSuffix' => '%',
                                 'valueDecimals' => 2
-                            ]
+                            ],
+                            'lineWidth' => 3
                         ]
                     ]
                 ])
@@ -150,21 +218,69 @@ class CurrencyCorrelationDetailsScreen extends Screen
             ->get();
 
         $priceData = [];
-        $btcCorrelationData = [];
-        $ethCorrelationData = [];
+        // BTC корреляции
+        $btcCorrelation4h = [];
+        $btcCorrelation12h = [];
+        $btcCorrelation24h = [];
+        $btcCorrelationAvg = [];
+        
+        // ETH корреляции
+        $ethCorrelation4h = [];
+        $ethCorrelation12h = [];
+        $ethCorrelation24h = [];
+        $ethCorrelationAvg = [];
 
         foreach ($historicalData as $data) {
             $timestamp = $data->created_at->timestamp * 1000;
 
+            // Цена
             $priceData[] = [$timestamp, round($data->current_price, 8)];
-            $btcCorrelationData[] = [$timestamp, round($data->price_change_vs_btc_24h, 5)];
-            $ethCorrelationData[] = [$timestamp, round($data->price_change_vs_eth_24h, 5)];
+            
+            // BTC корреляции
+            $btc4h = round($data->price_change_vs_btc_4h, 5);
+            $btc12h = round($data->price_change_vs_btc_12h, 5);
+            $btc24h = round($data->price_change_vs_btc_24h, 5);
+            
+            $btcCorrelation4h[] = [$timestamp, $btc4h];
+            $btcCorrelation12h[] = [$timestamp, $btc12h];
+            $btcCorrelation24h[] = [$timestamp, $btc24h];
+            
+            // Среднее значение BTC (исключаем null значения)
+            $btcValues = array_filter([$btc4h, $btc12h, $btc24h], function($value) {
+                return $value !== null;
+            });
+            $btcAvg = count($btcValues) > 0 ? array_sum($btcValues) / count($btcValues) : null;
+            $btcCorrelationAvg[] = [$timestamp, $btcAvg];
+            
+            // ETH корреляции
+            $eth4h = round($data->price_change_vs_eth_4h, 5);
+            $eth12h = round($data->price_change_vs_eth_12h, 5);
+            $eth24h = round($data->price_change_vs_eth_24h, 5);
+            
+            $ethCorrelation4h[] = [$timestamp, $eth4h];
+            $ethCorrelation12h[] = [$timestamp, $eth12h];
+            $ethCorrelation24h[] = [$timestamp, $eth24h];
+            
+            // Среднее значение ETH (исключаем null значения)
+            $ethValues = array_filter([$eth4h, $eth12h, $eth24h], function($value) {
+                return $value !== null;
+            });
+            $ethAvg = count($ethValues) > 0 ? array_sum($ethValues) / count($ethValues) : null;
+            $ethCorrelationAvg[] = [$timestamp, $ethAvg];
         }
 
         return [
             'price' => $priceData,
-            'btcCorrelation' => $btcCorrelationData,
-            'ethCorrelation' => $ethCorrelationData
+            // BTC данные
+            'btcCorrelation4h' => $btcCorrelation4h,
+            'btcCorrelation12h' => $btcCorrelation12h,
+            'btcCorrelation24h' => $btcCorrelation24h,
+            'btcCorrelationAvg' => $btcCorrelationAvg,
+            // ETH данные
+            'ethCorrelation4h' => $ethCorrelation4h,
+            'ethCorrelation12h' => $ethCorrelation12h,
+            'ethCorrelation24h' => $ethCorrelation24h,
+            'ethCorrelationAvg' => $ethCorrelationAvg
         ];
     }
 }
