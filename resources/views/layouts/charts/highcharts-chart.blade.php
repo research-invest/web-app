b3a2a6bd6c0ce69dd89bf2b1c5c3de13fe4d7204
rocky <88kg@mail.ru>
2025-06-17 00:50:16 +0300

    торговые сессии

--- resources/views/layouts/charts/highcharts-chart.blade.php
+++ resources/views/layouts/charts/highcharts-chart.blade.php
@@ -2,7 +2,7 @@
     $idContainer = Illuminate\Support\Str::random(10);
 @endphp
 
-<div id="highcharts-container-{{ $idContainer }}" style="width: 100%;  min-height: 70vh;"></div>
+<div id="highcharts-container-{{ $idContainer }}" style="width: 100%;  min-height: 26vh;"></div>
 
 <script defer>
     function initializeChart_{{ $idContainer }}() {
