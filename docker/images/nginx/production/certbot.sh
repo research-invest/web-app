9916819e61a96543f8f5aec7d91b38aa1fe9cabe
rocky <88kg@mail.ru>
2025-06-18 12:24:28 +0300

    sert test

--- docker/images/nginx/production/certbot.sh
+++ docker/images/nginx/production/certbot.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# prod
-certbot certonly --webroot -w /var/www/web-app/public -d selll.ru --agree-tos -n --email hello@selll.ru
+certbot certonly --webroot -w /var/www/web-app/public -d selll.ru --agree-tos -n --email hello@selll.ru --force-renewal
 
 cp /etc/letsencrypt/live/selll.ru/fullchain.pem /etc/nginx/ssl/selll.ru.crt
 cp /etc/letsencrypt/live/selll.ru/privkey.pem /etc/nginx/ssl/selll.ru.key
 
-nginx -g 'daemon off;'
+nginx -s reload
